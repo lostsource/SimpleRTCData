52d69d642cae1e8b7069e5d2c50d2150d4d343bb
Joseph Portelli <joseph@lostsource.com>
2015-04-12 21:09:10 +0200

    Improved example. Now both peers can exchange messages.

--- example/js/ui.js
+++ example/js/ui.js
@@ -1,8 +1,11 @@
-window.addEventListener('load',function(){
+/* global  window, SimpleRTCData, console */
+'use strict';
+
+window.addEventListener('load', function() {
 
 	function showCode (readyCB, container, message, index) {    
 		// adapted from http://stackoverflow.com/a/7265613/149636 
-		if(typeof(index) === "undefined") {
+		if(typeof(index) === 'undefined') {
 			index = 0;
 		}
 
@@ -19,11 +22,11 @@ window.addEventListener('load',function(){
 
 	var code = {
 		bertsOffer: "var BertRTC = new SimpleRTCData;\nBertRTC.getOffer(function(offer) {\n  // 'offer' has to be sent to Ernie\n});",
-		erniesAnswer: "var ErnieRTC = new SimpleRTCData;\n\nErnieRTC.onChannelEvent('message',function(e) {\n  // get ready for messages from Bert \n  console.log(e.data);\n});\n\nErnieRTC.getAnswer(offer,function(answer) {\n  // 'answer' has to be sent to Bert\n});",
-		bertSetAnswer: "BertRTC.onChannelEvent('open',function(){\n  // Bert's ready to send messages\n\n});\n\nBertRTC.setAnswer(answer);"
-	}
+		erniesAnswer: "var ErnieRTC = new SimpleRTCData;\n\nErnieRTC.on('data',function(data) {\n  // get ready for messages from Bert \n  console.log(data);\n});\n\nErnieRTC.getAnswer(offer,function(answer) {\n  // 'answer' has to be sent to Bert\n});",
+		bertSetAnswer: "BertRTC.on('connect',function(){\n  // Bert's ready to send messages\n\n});\n\nBertRTC.setAnswer(answer);"
+	};
 
-	var BertRTC = null, BertChan = null, ErnieRTC = null;
+	var BertRTC = null, ErnieRTC = null;
 	var CreatingOffer = false, CreatingAnswer = false, SettingAnswer = false;
 
 	var elmBertWindow = document.getElementById('bertWindow');
@@ -46,25 +49,36 @@ window.addEventListener('load',function(){
 	var elmBertSendMsgPrefix = document.getElementById('bertSendMessagePrefix');
 	var elmBertSendMsgPostfix = document.getElementById('bertSendMessagePostfix');
 	var elmBertMsgNpt = document.getElementById('msgSender');
+	var elmErnieMsgNpt = document.getElementById('ernieMsgSender');
 
 	var elmMsgReceiver = document.getElementById('msgReceiver');
+	var elmBertMsgReceiver = document.getElementById('bertMsgReceiver');
 
-	// firefox keeps does not reset button states and textarea contents after refresh
+	var elmErnieSendMsgPrefix = document.getElementById('ernieSendMessagePrefix');
+	var elmErnieSendMsgPostfix = document.getElementById('ernieSendMessagePostfix');
+
+	var elmBertMsgSender = document.getElementById('bertSendMsg');
+
+
+	// firefox does not reset button states and textarea contents after refresh
 	elmCreateOffer.disabled = false; 
-	elmErnieOfferHolder.value = "";
-	elmBertsAnswerHolder.value = "";
-	elmErnieAnswerHolder.value = "";
-	elmOfferResult.value = "";
+	elmErnieOfferHolder.value = '';
+	elmBertsAnswerHolder.value = '';
+	elmErnieAnswerHolder.value = '';
+	elmOfferResult.value = '';
 
 
 	elmBertMsgNpt.addEventListener('keydown',function(e){
-		if(!BertChan) {
-			return;
-		}
-
 		if(e.keyCode === 13) {
-			BertChan.send(this.value);
-			this.value = "";
+			BertRTC.send(this.value);
+			this.value = '';
+		}
+	});
+
+	elmErnieMsgNpt.addEventListener('keydown',function(e){
+		if(e.keyCode === 13) {
+			ErnieRTC.send(this.value);
+			this.value = '';
 		}
 	});
 
@@ -76,15 +90,15 @@ window.addEventListener('load',function(){
 		}
 		CreatingOffer = true;
 
-		elmBertsOffer.innerHTML = "";
+		elmBertsOffer.innerHTML = '';
 
 		showCode(function(){
-			BertRTC = new SimpleRTCData;
-			BertRTC.on("connect", function(e) {
+			BertRTC = new SimpleRTCData();
+			BertRTC.on('connect', function() {
 				console.log("%cBert:  on('connect')", "color:blue");
 			});
 
-			BertRTC.on("disconnect", function(e) {
+			BertRTC.on('disconnect', function() {
 				console.log("%cBert:  on('disconnect')", "color:blue");
 			});
 
@@ -101,6 +115,46 @@ window.addEventListener('load',function(){
 			BertRTC.onChannelEvent('*',function(e){
 				console.log("%cBert:  onChannelEvent ["+e.type+"]", "color:blue");
 			});					
+
+			BertRTC.on('data',function(data) {
+				elmBertMsgReceiver.innerHTML = '';
+				elmBertMsgReceiver.appendChild(document.createTextNode(data));
+				elmBertMsgReceiver.style.opacity = 1;
+
+				// get ready for messages from Bert 
+//				elmMsgReceiver.innerHTML = '';
+//				elmMsgReceiver.appendChild(document.createTextNode(data));
+//				elmMsgReceiver.style.opacity = 1;
+
+				elmBertWindow.addEventListener('transitionend',function(e){
+					if(e.propertyName === "height") {
+						elmBertMsgSender.style.visibility = "visible";
+
+
+
+						/*
+						setTimeout(function() {
+
+							elmErnieSendMsgPrefix.style.visibility = "visible";
+							elmErnieSendMsgPrefix.style.backgroundColor = 'transparent';
+							elmErnieSendMsgPostfix.style.visibility = "visible";
+							elmErnieSendMsgPostfix.style.backgroundColor = 'transparent';
+
+							elmErnieMsgNpt.style.left = ((elmBertSendMsgPrefix.offsetLeft+elmBertSendMsgPrefix.offsetWidth)-6)+"px";
+							elmErnieMsgNpt.style.backgroundColor = 'transparent';
+							elmErnieSendMsgPostfix.style.left = ((elmBertSendMsgPrefix.offsetLeft+elmBertSendMsgPrefix.offsetWidth+elmErnieMsgNpt.offsetWidth))+"px";
+
+							elmErnieMsgNpt.style.visibility = "visible";
+							elmErnieMsgNpt.focus();
+
+
+						},500);
+*/
+					}
+				});
+
+				elmBertWindow.classList.add('messageSender');
+			});
 
 
 			elmBertWindow.classList.add('execWait');
@@ -136,7 +190,7 @@ window.addEventListener('load',function(){
 
 			elmBertWindow.classList.add('settingAnswer');
 			showCode(function(){
-				BertRTC.onChannelEvent('open',function(){
+				BertRTC.on('connect',function(){
 					// triggered when we're read to send a message
 					elmBertSendMsgPrefix.style.visibility = "visible";
 					elmBertSendMsgPrefix.style.backgroundColor = 'transparent';
@@ -148,7 +202,6 @@ window.addEventListener('load',function(){
 
 					elmBertMsgNpt.style.visibility = "visible";
 					elmBertMsgNpt.focus();
-					BertChan = this;
 					elmBertsAnswerHolder.readOnly = true;
 					elmBertWindow.classList.remove('execWait');
 				});
@@ -158,6 +211,12 @@ window.addEventListener('load',function(){
 				BertRTC.setAnswer(answerVal, function(detail) {
 					if(detail.error) {
 						console.warn(detail.error);
+					}
+					else {
+						if(elmErnieOfferHolder.value.trim().length === 0) {
+							elmErnieWindow.classList.add('inactive');
+							elmErnieOfferHolder.disabled = true;
+						}
 					}
 				});	
 
@@ -185,19 +244,24 @@ window.addEventListener('load',function(){
 
 		CreatingAnswer = true;
 
-		elmErniesAnswer.innerHTML = "";
+		elmErniesAnswer.innerHTML = '';
 
 		elmErnieWindow.classList.add('writingAnswerCode');
 
 		showCode(function(){
 
 			ErnieRTC = new SimpleRTCData;
-			ErnieRTC.on("connect", function(e) {
+			ErnieRTC.on("connect", function() {
 				console.log("%cErnie: on('connect')", "color:maroon");
 			});
 
-			ErnieRTC.on("disconnect", function(e) {
+			ErnieRTC.on("disconnect", function() {
 				console.log("%cErnie: on('disconnect')", "color:maroon");
+			});
+
+
+			ErnieRTC.on("data", function() {
+				console.log("%cErnie: on('data')", "color:maroon");
 			});
 
 			var ErnieConn = ErnieRTC.getConnection();
@@ -216,13 +280,36 @@ window.addEventListener('load',function(){
 
 			ErnieRTC.onError = function(error) {
 				console.warn(error);
-			}
+			};
 
-			ErnieRTC.onChannelEvent('message',function(e) {
+			ErnieRTC.on('data',function(data) {
 				// get ready for messages from Bert 
-				elmMsgReceiver.innerHTML = "";
-				elmMsgReceiver.appendChild(document.createTextNode(e.data));
+				elmMsgReceiver.innerHTML = '';
+				elmMsgReceiver.appendChild(document.createTextNode(data));
 				elmMsgReceiver.style.opacity = 1;
+
+				elmErnieWindow.addEventListener('transitionend',function(e){
+					if(e.propertyName === "height") {
+						setTimeout(function() {
+
+							elmErnieSendMsgPrefix.style.visibility = "visible";
+							elmErnieSendMsgPrefix.style.backgroundColor = 'transparent';
+							elmErnieSendMsgPostfix.style.visibility = "visible";
+							elmErnieSendMsgPostfix.style.backgroundColor = 'transparent';
+
+							elmErnieMsgNpt.style.left = ((elmBertSendMsgPrefix.offsetLeft+elmBertSendMsgPrefix.offsetWidth)-6)+"px";
+							elmErnieMsgNpt.style.backgroundColor = 'transparent';
+							elmErnieSendMsgPostfix.style.left = ((elmBertSendMsgPrefix.offsetLeft+elmBertSendMsgPrefix.offsetWidth+elmErnieMsgNpt.offsetWidth))+"px";
+
+							elmErnieMsgNpt.style.visibility = "visible";
+							elmErnieMsgNpt.focus();
+
+
+						},200);
+					}
+				});
+
+				elmErnieWindow.classList.add('messageSender');
 			});
 
 			elmErnieWindow.classList.add('execWait');
@@ -233,6 +320,11 @@ window.addEventListener('load',function(){
 				elmErnieOfferHolder.readOnly = true;
 				elmErnieWindow.classList.remove('execWait');
 				elmErnieWindow.classList.add('showingAnswer');
+
+				if(elmOfferResult.value.trim().length === 0) {
+					elmBertWindow.classList.add("inactive");
+					elmCreateOffer.disabled = true;
+				}
 			});
 
 		},elmErniesAnswer,code.erniesAnswer);
